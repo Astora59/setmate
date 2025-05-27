86ceec607d8ac85304a5d8e5559199cf8126e985
Yamin <yaminmoulay12@gmail.com>
2025-05-27 15:03:25 +0200

    installed react bootstrap library

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,15 +1,17 @@
 import classes from "./Header.module.scss"
+import logo from "../../assets/logo_setmate.svg"
+import { Button } from 'react-bootstrap';
 
 export default function Header() {
     return (
         <header>
             <nav className={classes.navbar}>
                 <div>
-                    <img src= "./assets/logo_setmate.svg" alt="logo setmate"/>
+                    <img src= {logo} alt="logo setmate" className={classes.logo}/>
                 </div>
                 <ul>
                     <li><a href="#">LAUNCHES</a></li>
-                    
+                    <Button variant="primary">Clique-moi</Button>;
                 </ul>
             </nav> 
         </header>
