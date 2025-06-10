bd599f33643e559851864f1c1a64a620e1082f5c
Yamin <yaminmoulay12@gmail.com>
2025-06-10 16:58:04 +0200

    Finished homepage

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,39 +1,35 @@
-const Footer = () => <footer className="page-footer font-small blue pt-4">
-    <div className="container-fluid text-center text-md-left">
-        <div className="row">
-            <div className="col-md-6 mt-md-0 mt-3">
-                <h5 className="text-uppercase">Footer Content</h5>
-                <p>Here you can use rows and columns to organize your footer content.</p>
+import twitter from "../../assets/twitter.svg"
+import facebook from "../../assets/facebook.svg"
+import discord from "../../assets/discord.svg"
+import instagram from "../../assets/insta.svg"
+
+import classes from "./Footer.module.scss"
+
+export default function Footer() {
+    return (
+        <footer className={classes.footer}>
+            <div className={classes.footer_container}>
+                <div className={classes.footer_content_left}>
+                    <h3>Setmate</h3>
+                    <p className={classes.footer_text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare cursus sed nunc eget dictum  Sed ornare cursus sed nunc eget dictumd nunc eget dictum  Sed ornare cursus sed nunc eget dictum  </p>
+                    <div className={classes.footer_socials}>
+                        <img src={twitter} alt="" />
+                        <img src={facebook} alt="" />
+                        <img src={discord} alt="" />
+                        <img src={instagram} alt="" />
+                    </div>
+                </div>
+                <div className={classes.footer_content_right}>
+                    <h3>Liens utiles</h3>
+                    <ul className={classes.footer_links}>
+                        <li><a href="#">Accueil</a></li>
+                        <li><a href="#">Concept</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Se connecter</a></li>
+                        <li><a href="#">S'inscrire</a></li>
+                    </ul>
+                </div>
             </div>
-
-            <hr className="clearfix w-100 d-md-none pb-0"/>
-
-            <div className="col-md-3 mb-md-0 mb-3">
-                <h5 className="text-uppercase">Links</h5>
-                <ul className="list-unstyled">
-                    <li><a href="#!">Link 1</a></li>
-                    <li><a href="#!">Link 2</a></li>
-                    <li><a href="#!">Link 3</a></li>
-                    <li><a href="#!">Link 4</a></li>
-                </ul>
-            </div>
-
-            <div className="col-md-3 mb-md-0 mb-3">
-                <h5 className="text-uppercase">Links</h5>
-                <ul className="list-unstyled">
-                    <li><a href="#!">Link 1</a></li>
-                    <li><a href="#!">Link 2</a></li>
-                    <li><a href="#!">Link 3</a></li>
-                    <li><a href="#!">Link 4</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-
-    <div className="footer-copyright text-center py-3">© 2020 Copyright:
-        <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
-    </div>
-
-</footer>
-
-export default Footer
+        </footer>
+    );
+}
