195ad9352cb2350c9846484f43626186eda7b26c
Yamin <yaminmoulay12@gmail.com>
2025-06-23 15:36:01 +0200

    working on login page style

--- frontend/src/pages/AccountCreation/AccountCreation.tsx
+++ frontend/src/pages/AccountCreation/AccountCreation.tsx
@@ -1,28 +1,35 @@
 import logo from "../../assets/logo_setmate.svg"
 import classes from "./AccountCreation.module.scss"
+import login from "../../assets/login.gif"
 
 export default function AccountCreation() {
     return (
-        <section>
-            <img src={logo} className={classes.logo}alt="logo setmate" />
+        <section className={classes.container}>
             <div>
-                <h2>Content de vous revoir !</h2>
-                <div>
-                    <p>Login</p>
-                    <input type="text" />
-                </div>
-
-                <div>
-                    <p>Mot de passe</p>
-                    <input type="password" />
-                </div>
-                <div>
-                    <button>Se souvenir de moi</button>
-                    <p>Mot de passe oublié ?</p>
-                </div>
-
-
+                <img src={login} alt="" />
             </div>
+            <div>
+                <img src={logo} className={classes.logo}alt="logo setmate" />
+                <div>
+                    <h2>Content de vous revoir !</h2>
+                    <div>
+                        <p>Login</p>
+                        <input type="text" />
+                    </div>
+
+                    <div>
+                        <p>Mot de passe</p>
+                        <input type="password" />
+                    </div>
+                    <div>
+                        <button>Se souvenir de moi</button>
+                        <p>Mot de passe oublié ?</p>
+                    </div>
+
+
+                </div>
+            </div>
+            
         </section>
     )
 }
